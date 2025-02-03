4b5b31bd3694adbd9e12111674f01e32332dcb86
Bread <dkffbk@gmail.com>
2025-02-03 17:31:19 +0630

    Just for memory

---
+++ app/js/cjs/b.js
@@ -0,0 +1,9 @@
+// const a = require("./a");
+
+// console.log(a.PI);
+// console.log(a.add(2, 3));
+
+const { PI, add } = require("./a");
+
+console.log(PI);
+console.log(add(1, 2));
---
+++ app/js/esm/b.mjs
@@ -0,0 +1,10 @@
+// import * as a from "./a.mjs";
+
+// console.log(a.PI);
+// console.log(a.add(4, 2));
+// console.log(a.sub(7,3));
+
+import { PI, add } from "./a.mjs";
+
+console.log(PI);
+console.log(add(3, 4));
